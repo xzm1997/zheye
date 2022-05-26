91df8083ff0b328fca6b8ef417db581fc18f1046
Poco <xuzhiming1997@163.com>
2022-05-26 09:34:54 +0800

    add

---
+++ .history/src/store_20220526093442.ts
@@ -0,0 +1,95 @@
+import { createStore, Commit } from 'vuex'
+import axios from 'axios'
+
+interface UserProps {
+  isLogin: boolean;
+  name?: string;
+  id?: number;
+  columnId?: number;
+}
+
+interface ImageProps {
+  _id?: string;
+  url?: string;
+  createdAt?: string;
+}
+
+export interface ColumnProps {
+  _id: string;
+  title: string;
+  avatar?: ImageProps;
+  description: string;
+}
+
+export interface PostProps {
+  _id: string;
+  title: string;
+  excerpt?: string;
+  content?: string;
+  image?: ImageProps;
+  createdAt: string;
+  column: string;
+}
+
+export interface GlobalDataProps {
+  loading: boolean
+  columns: ColumnProps[];
+  posts: PostProps[];
+  user: UserProps;
+}
+
+const getAndCommit = async (url: string, mutationName: string, commit: Commit) => {
+  const { data } = await axios.get(url)
+  commit(mutationName, data)
+}
+
+const store = createStore<GlobalDataProps>({
+  state: {
+    loading: false,
+    columns: [],
+    posts: [],
+    user: { isLogin: false, name: 'viking', columnId: 1 }
+  },
+  mutations: {
+    login (state) {
+      state.user = { ...state.user, isLogin: true, name: 'viking' }
+    },
+    createPost (state, newPost) {
+      state.posts.push(newPost)
+    },
+    fetchColumns (state, rawData) {
+      state.columns = rawData.data.list
+    },
+    fetchColumn (state, rawData) {
+      state.columns = [rawData.data]
+    },
+    fetchPosts (state, rawData) {
+      state.posts = rawData.data.list
+    },
+    setLoading (state, status) {
+      state.loading = status
+    }
+  },
+  actions: {
+    async fetchColumns ({ commit }) {
+      getAndCommit('/columns', 'fetchColumns', commit)
+    },
+    async fetchColumn ({ commit }, cid) {
+      getAndCommit(`/columns/${cid}`, 'fetchColumn', commit)
+    },
+    async fetchPosts ({ commit }, cid) {
+      getAndCommit(`/columns/${cid}/post`, 'fetchPosts', commit)
+    }
+  },
+  getters: {
+    getColumnById: (state) => (id: string) => {
+      return state.columns.find(c => c._id === id)
+    },
+    getPostsByCid: (state) => (cid: string) => {
+      return state.posts
+      // return state.posts.filter(post => post.column === cid)
+    }
+  }
+})
+
+export default store
