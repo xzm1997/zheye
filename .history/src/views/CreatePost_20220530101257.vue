1a33b81790d446a31f811ec0d0fff423a5cc5f65
Poco <xuzhiming1997@163.com>
2022-05-30 10:24:50 +0800

    add

---
+++ .history/src/views/CreatePost_20220530101257.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="create-post-page">
+    <h4>新建文章</h4>
+    <up-loader
+      action="/upload"
+      :beforeUpload="uploadCheck"
+      @file-uploaded="handleFileUploaded"
+      class="d-flex align-items-center justify-content-center bg-light text-secondary w-100 my-4"
+    >
+      <h2>点击上传头图</h2>
+      <template #loading>
+        <div class="d-flex">
+          <div class="spinner-border text-secondary" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
+          <h2>正在上传</h2>
+        </div>
+      </template>
+      <template #uploaded="dataProps">
+        <img :src="dataProps.uploadedData.data.url">
+      </template>
+    </up-loader>
+    <validate-form @form-submit="onFormSubmit">
+      <div class="mb-3">
+        <label class="form-label">文章标题：</label>
+        <validate-input
+          :rules="titleRules" v-model="titleVal"
+          placeholder="请输入文章标题"
+          type="text"
+        />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">文章详情：</label>
+        <validate-input
+          rows="10"
+          tag="textarea"
+          placeholder="请输入文章详情"
+          :rules="contentRules"
+          v-model="contentVal"
+        />
+      </div>
+      <template #submit>
+        <button class="btn btn-primary btn-large">发表文章</button>
+      </template>
+    </validate-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+import { GlobalDataProps, PostProps, ResponseType, ImageProps } from '../store'
+import ValidateInput, { RulesProp } from '../components/ValidateInput.vue'
+import ValidateForm from '../components/ValidateForm.vue'
+import axios from 'axios'
+import UpLoader from '../components/UpLoader.vue'
+import createMessage from '../components/createMessage'
+import { beforeUploadCheck } from '../helper'
+export default defineComponent({
+  components: {
+    ValidateInput,
+    ValidateForm,
+    UpLoader
+  },
+  setup () {
+    const titleVal = ref('')
+    const router = useRouter()
+    const store = useStore<GlobalDataProps>()
+    let imageId = ''
+    const titleRules: RulesProp = [
+      { type: 'required', message: '文章标题不能为空' }
+    ]
+    const contentVal = ref('')
+    const contentRules: RulesProp = [
+      { type: 'required', message: '文章详情不能为空' }
+    ]
+    const handleFileUploaded = (rawData: ResponseType<ImageProps>) => {
+      if (rawData.data._id) {
+        imageId = rawData.data._id
+      }
+    }
+    const onFormSubmit = (result: boolean) => {
+      if (result) {
+        const { column, _id } = store.state.user
+        if (column) {
+          const newPost: PostProps = {
+            _id: new Date().getTime().toString(),
+            title: titleVal.value,
+            content: contentVal.value,
+            column,
+            createdAt: new Date().toLocaleString(),
+            author: _id
+          }
+          if (imageId) {
+            newPost.image = imageId
+          }
+          store.dispatch('createPost', newPost).then(() => {
+            createMessage('发表成功，2秒后跳转到文章', 'success', 2000)
+            setTimeout(() => {
+              router.push({ name: 'column', params: { id: column } })
+            }, 2000)
+          })
+        }
+      }
+    }
+    const handleFileChange = (e: Event) => {
+      const target = e.target as HTMLInputElement
+      console.log(target)
+      const files = target.files
+      if (files) {
+        const uploadedFile = files[0]
+        const formData = new FormData()
+        formData.append(uploadedFile.name, uploadedFile)
+        axios.post('/upload', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        }).then((resp: any) => {
+          console.log(resp)
+        })
+      }
+    }
+    const uploadCheck = (file: File) => {
+      const result = beforeUploadCheck(file, { format: ['image/jpeg', 'image/png'], size: 1 })
+      const { passed, error } = result
+      if (error === 'format') {
+        createMessage('上传图片只能是 JPG/PNG 格式', 'error')
+      }
+      if (error === 'size') {
+        createMessage('上传图片大小不能超过 1Mb', 'error')
+      }
+      return passed
+    }
+    return {
+      router,
+      store,
+      titleVal,
+      titleRules,
+      contentVal,
+      contentRules,
+      onFormSubmit,
+      handleFileChange,
+      uploadCheck,
+      handleFileUploaded
+    }
+  }
+})
+</script>
+
+<style>
+.create-post-page .file-upload-container {
+  height: 200px;
+  cursor: pointer;
+}
+.create-post-page .file-upload-container img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
